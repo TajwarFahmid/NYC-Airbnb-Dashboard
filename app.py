e81c9cad4e8dfa554e797ff13c93d49894d54601
Tajwar Fahmid <123596299+TajwarFahmid@users.noreply.github.com>
2026-04-09 00:28:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Makes the dashboard fill the whole screen width
+st.set_page_config(layout="wide")
+
+st.title("NYC Airbnb Dashboard")
+
+# Replace the URL below with your actual Tableau Share Link
+# Make sure it has ?:embed=yes at the end!
+tableau_url = "https://public.tableau.com/views/project_17756312871780/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link?:embed=yes"
+
+components.iframe(tableau_url, height=800, scrolling=True)
